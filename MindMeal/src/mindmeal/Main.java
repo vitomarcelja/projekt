68009554d222d974ca2362e53ecd60300f5d0c40
Vito <Vito@DESKTOP-6H96UQM>
2024-06-06 21:15:46 +0200

    Prvi commit

---
+++ MindMeal/src/mindmeal/Main.java
@@ -0,0 +1,55 @@
+package mindmeal;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Object[] options = {"Korisnik", "Stručnjak"};
+            int choice = JOptionPane.showOptionDialog(null,
+                    "Dobrodošli u MindMeal aplikaciju!\nMolimo prijavite se kako biste nastavili.",
+                    "Prijava",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    options,
+                    options[0]);
+
+            if (choice == JOptionPane.YES_OPTION) {
+                String email = JOptionPane.showInputDialog(null, "Upišite svoj email:");
+                String password = JOptionPane.showInputDialog(null, "Upišite svoju lozinku:");
+                if (validateUserCredentials(email, password)) {
+                    openUserInterface();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Pogrešan email ili lozinka!");
+                }
+            } else if (choice == JOptionPane.NO_OPTION) {
+                String email = JOptionPane.showInputDialog(null, "Upišite svoj email:");
+                String password = JOptionPane.showInputDialog(null, "Upišite svoju lozinku:");
+                if (validateExpertCredentials(email, password)) {
+                    openExpertInterface();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Pogrešan email ili lozinka!");
+                }
+            }
+        });
+    }
+
+    private static boolean validateUserCredentials(String email, String password) {
+        return true; 
+    }
+
+    private static boolean validateExpertCredentials(String email, String password) {
+        return true; 
+    }
+
+    private static void openUserInterface() {
+        Korisnik krsnkOdabir = new Korisnik();
+        krsnkOdabir.setVisible(true);
+    }
+
+    private static void openExpertInterface() {
+        Stručnjak stručnjak = new Stručnjak();
+        stručnjak.setVisible(true);
+    }
+}
